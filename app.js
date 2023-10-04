d01df31d94c7d7cb9a349eee6ade49d49661823e
jungYoungSick <junfas1216@gmail.comgit config --global user.email junfas1216@gmail.com>
2023-10-04 12:09:02 +0900

    라이브서버 동작을 수동으로 작업

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello')
+// 서버 만들기
+
+const http = require('http');
+//이미 모듈이 깔려 있다는 전제로 작성.
+
+http.createServer(function(request, response){
+let writeHeadObject = {
+  'Content-Type': 'application/json'
+}
+
+  response.writeHead(200, writeHeadObject);
+  response.end("hello")
+
+
+}).listen(8080);
+//매개변수는 콜백 함수만 받는다. 읽기 좋게 구분해서 써주도록 하자.
+
